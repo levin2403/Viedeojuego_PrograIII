9af2a02f03b74351471954d5fedf8e3612e2e4b0
KImberliMartinez <159958568+KImberliMartinez@users.noreply.github.com>
2024-04-24 22:02:03 -0700

    base

---
+++ videojuego/src/videojuego/Juego.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package videojuego;
+
+/**
+ *
+ * @author delll
+ */
+public class Juego {
+     Fondo fondo=new Fondo(this);
+     Personaje personaje=new Personaje(this);
+}

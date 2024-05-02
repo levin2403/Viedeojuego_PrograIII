58f3d6260d3851b1d0eaa42dad47026d074d8f79
KImberliMartinez <159958568+KImberliMartinez@users.noreply.github.com>
2024-05-02 10:36:23 -0700

    musica

--- JuegoDesde0/src/Frms/Main.java
+++ JuegoDesde0/src/Frms/Main.java
@@ -4,12 +4,25 @@
  */
 package Frms;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 /**
  *
  * @author USER
  */
 public class Main {
     public static void main(String[] args) {
+            try{
+                Clip clip=AudioSystem.getClip();
+                AudioInputStream inputStream=AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("/Sonidos/musicaJuego.wav"));
+            clip.open(inputStream);
+            
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }catch(Exception e){
+                System.err.println("error al reproducir la musica"+e.getMessage());
+            }
         PantallaPrincipal p=new PantallaPrincipal();
     }
 }
